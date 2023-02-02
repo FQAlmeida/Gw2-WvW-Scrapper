bbea3dd465e063dc67520dd23ca2e60b37427659
FQA <otavioalmeida650@gmail.com>
2023-02-02 17:40:08 -0300

    arg for outdir(arg/data/), create outdir if needed

--- gw2-wvw-scrapper/src/main.rs
+++ gw2-wvw-scrapper/src/main.rs
@@ -1,19 +1,26 @@
 use gw2_api_wrapper::Gw2ApiWrapper;
 use tokio_cron_scheduler::{Job, JobScheduler};
+use std::env;
 
 #[tokio::main]
 async fn main() {
     let scheduler = JobScheduler::new().await.unwrap();
 
-    let job = Job::new_async("0 0,59 1,2,17,18 * * *", |_, _| {
-        Box::pin(async move{
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let default_basepath = String::from(".");
+    let basepath = args.get(1).unwrap_or(&default_basepath).clone();
+
+    let job = Job::new_async("0 1/1 * * * *", move |_, _| {
+        let this_basepath = basepath.clone();
+        Box::pin(async move {
             dbg!("Running Job");
             let api = Gw2ApiWrapper::create();
             let ids = api.get_matchup_ids().await.unwrap();
             dbg!(&ids);
             let info = api.get_matchup_info(ids).await.unwrap();
-            dbg!(&info);
-            gw2_info_persistence::save(info).unwrap();
+            // dbg!(&info);
+            gw2_info_persistence::save(&this_basepath, info).unwrap();
             dbg!("Saved");
         })
     })
