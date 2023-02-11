93ceaa8a72cfe568e601a60beb54f4e53791c460
FQA <otavioalmeida650@protonmail.ch>
2023-02-11 02:16:59 -0300

    datetime type for endpoint filter

--- gw2-info-api/src/main.rs
+++ gw2-info-api/src/main.rs
@@ -1,7 +1,27 @@
-#[macro_use] extern crate rocket;
+use chrono::NaiveDate;
+use chrono::ParseError;
 
-#[get("/")]
-fn index() -> &'static str {
+use rocket::get;
+use rocket::launch;
+use rocket::request::FromParam;
+use rocket::routes;
+
+pub struct NaiveDateForm(pub NaiveDate);
+
+
+impl<'a> FromParam<'a> for NaiveDateForm {
+    type Error = ParseError;
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error>{
+      match NaiveDate::parse_from_str(&param, "%Y-%m-%d") {
+          Ok(date)=> Ok(NaiveDateForm(date)),
+          Err(e) =>Err(e),
+        }
+    }
+}
+
+#[get("/<start_date>/<end_date>")]
+fn index(start_date: NaiveDateForm,  end_date: NaiveDateForm) -> &'static str {
     "Hello, world!"
 }
 
